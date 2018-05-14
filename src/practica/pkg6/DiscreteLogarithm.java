d1dd5818a9226a8471f21c3e66bf4617ef91e121
Hazel Martinez <hazelmsr@gmail.com>
2018-05-14 02:49:15 -0500

    i as a BigInteger value

--- src/practica/pkg6/DiscreteLogarithm.java
+++ src/practica/pkg6/DiscreteLogarithm.java
@@ -2,21 +2,30 @@
 package practica.pkg6;
 import java.math.*;
 public class DiscreteLogarithm {
+    public static BigInteger pow(BigInteger base, BigInteger exponent) {
+  BigInteger result = BigInteger.ONE;
+  while (exponent.signum() > 0) {
+    if (exponent.testBit(0)) result = result.multiply(base);
+    base = base.multiply(base);
+    exponent = exponent.shiftRight(1);
+  }
+  return result;
+}
     public static int solve(BigInteger number, BigInteger mod, BigInteger result){
-        int i = 1;
+        BigInteger i = new BigInteger("1");
         boolean flag = false;
         int res = 1;
         int res2 = 1;
         while(res!=0 || flag == false){
-            res = BigInteger.valueOf(i).compareTo(mod);
-            res2 = result.compareTo((number.pow(i)).mod(mod));
+            res = i.compareTo(mod);
+            res2 = result.compareTo((pow(number,i)).mod(mod));
             if(res2 == 0){
                 flag = true;
-                return i;
+                return i.intValue();
             }
             System.out.println(i);
-            i++;   
+            i=i.add(new BigInteger("1"));   
         }
-        return i-1;
+        return i.subtract(new BigInteger("1")).intValue();
     }
 }
